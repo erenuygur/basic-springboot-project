20310fc5b5bfa4f93954ebad7022662dc3ac2637
Eren <ernuygur@gmail.com>
2023-06-28 01:56:07 +0300

    final commit

--- src/main/java/com/erenuygur/demo/student/StudentConfig.java
+++ src/main/java/com/erenuygur/demo/student/StudentConfig.java
@@ -14,10 +14,11 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            Student eren = new Student("Eren", "erenuygur@gmail.com", LocalDate.of(1999, Month.SEPTEMBER, 9), 23);
-            Student mehmet = new Student("Mehmet", "mehmet123@gmail.com", LocalDate.of(1993, Month.MARCH, 3), 30);
+            Student eren = new Student("Eren", "eren123@gmail.com", LocalDate.of(1999, Month.SEPTEMBER, 9));
+            Student mehmet = new Student("Mehmet", "mehmet123@gmail.com", LocalDate.of(1997, Month.MARCH, 3));
+            Student yasin = new Student("Yasin", "yasin123@gmail.com", LocalDate.of(1999, Month.MARCH, 8));
 
-            repository.saveAll(List.of(eren, mehmet));
+            repository.saveAll(List.of(eren, mehmet, yasin));
         };
     }
 }
